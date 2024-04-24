fb16a96bab5b76d63fc4d1db32eda3d1b3ce3c19
quocanh13 <nguyenvan2308199@gmail.com>
2024-04-24 09:25:21 +0700

    1

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let no = document.getElementById("no");
+let yes = document.getElementById("yes");
 let AfterFrame = document.getElementById("afterframe");
 let heart = document.getElementsByClassName("Heart");
 let AfterMessage = document.getElementById("AfterMessage");
+let ScreenWidth = screen.width;
+let ScreenHeight = screen.height;
+
+if(ScreenWidth < ScreenHeight)
+{
+    no.style.width = "10vw";
+    yes.style.width = "10vw";
+    no.style.height = "3vh";
+    yes.style.height = "3vh";
+    for(const i of heart)
+    {
+        getComputedStyle(i).animationDuration = parseInt(getComputedStyle(i).animationDuration) - 2 + "s";
+    }    
+}
 
 function ChangePosition()
 {
@@ -18,3 +33,4 @@ function Active()
         i.style.animationName = "Heart1";
     }
 }
+
